8bef5514abfe655e7be3d382be4296b9ef33e933
Documenter.jl <documenter@juliadocs.github.io>
2025-06-07 22:31:57 +0000

    build based on d4fad24

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Kabsch","page":"Home","title":"Kabsch","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Kabsch.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
